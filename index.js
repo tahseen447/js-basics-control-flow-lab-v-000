8b002d0a69addb00fd07003c1b60df6915920d09
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-11-07 00:12:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your code in this file!
+function scuberGreetingForFeet(ride) {
+  if (ride <= 400)
+  return "This one is on me!";
+  else if (ride > 2000 && ride < 2500)
+  return 'I will gladly take your thirty bucks.';
+  else
+  return 'No can do.';
+
+}
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+}
+
+function switchOnCharmFromTip() {
+  switch (tip) {
+  case 'generous':
+    return 'Thank you so much.';
+  case 'not as generous':
+    return 'Thank you.';
+  default:
+    return 'Bye.';
+}
+}
